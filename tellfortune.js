f7e0cd93e7e601f6d102237b72850d41fd48e90d
kendythegeek <chantellekyendereta@gmail.com>
2019-11-19 11:27:29 +0300

    fortunetelling

---
+++ tellfortune.js
@@ -0,0 +1,6 @@
+const tellFortune = (n ,z ,x ,y ) =>{
+    return (`You will be a ${y} in ${x}, and married to ${z} with ${n} kids.`);
+} 
+console.log(tellFortune(2 ,"Luka Sabbat", "Broadways", "model"))
+console.log(tellFortune(4, "Aubrey Graham", "Miami", "musician"))
+console.log(tellFortune(3, "Jace Norman", "Beverly Hills", "producer"))
